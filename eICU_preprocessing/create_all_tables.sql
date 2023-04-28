638b654d44f81d9e7d446d5d78767d3f65c94d39
@datagnomes <nomesg2020@gmail.com>
2023-04-28 11:59:40 -0400

    changed alll paths

--- eICU_preprocessing/create_all_tables.sql
+++ eICU_preprocessing/create_all_tables.sql
@@ -1,10 +1,16 @@
 -- creates all the tables and produces csv files
 -- takes a few minutes to run
+-- should be run as user asgnxt rather than eicu based on what worked for me
+-- this script worked on 4/27/2023 using :
+-- conda environment: pytorch-gpu
+-- switched to directory : /Users/asgnxt/hcnet/eICU-GNN-LSTM
+-- connect to psql server as: psql 'dbname=eicudemo user=asgnxt options=--search_path=public'
+-- then run this script as: \i eICU_preprocessing/create_all_tables.sql
 
-\i /Users/emmarocheteau/PycharmProjects/catherine/eICU_preprocessing/labels.sql
-\i /Users/emmarocheteau/PycharmProjects/catherine/eICU_preprocessing/diagnoses.sql
-\i /Users/emmarocheteau/PycharmProjects/catherine/eICU_preprocessing/flat_features.sql
-\i /Users/emmarocheteau/PycharmProjects/catherine/eICU_preprocessing/timeseries.sql
+\i /Users/asgnxt/hcnet/eICU-GNN-LSTM/eICU_preprocessing/labels.sql
+\i /Users/asgnxt/hcnet/eICU-GNN-LSTM/eICU_preprocessing/diagnoses.sql
+\i /Users/asgnxt/hcnet/eICU-GNN-LSTM/eICU_preprocessing/flat_features.sql
+\i /Users/asgnxt/hcnet/eICU-GNN-LSTM/eICU_preprocessing/timeseries.sql
 
 -- we need to make sure that we have at least some form of time series for every patient in diagnoses, flat and labels
 drop materialized view if exists timeseries_patients cascade;
@@ -24,10 +30,10 @@ create materialized view timeseries_patients as
   select distinct patientunitstayid
     from repeats;
 
-\copy (select * from labels as l where l.patientunitstayid in (select * from timeseries_patients)) to '/Users/emmarocheteau/PycharmProjects/catherine/eICU_data/labels.csv' with csv header
-\copy (select * from diagnoses as d where d.patientunitstayid in (select * from timeseries_patients)) to '/Users/emmarocheteau/PycharmProjects/catherine/eICU_data/diagnoses.csv' with csv header
-\copy (select * from flat as f where f.patientunitstayid in (select * from timeseries_patients)) to '/Users/emmarocheteau/PycharmProjects/catherine/eICU_data/flat_features.csv' with csv header
-\copy (select * from timeserieslab) to '/Users/emmarocheteau/PycharmProjects/catherine/eICU_data/timeserieslab.csv' with csv header
-\copy (select * from timeseriesresp) to '/Users/emmarocheteau/PycharmProjects/catherine/eICU_data/timeseriesresp.csv' with csv header
-\copy (select * from timeseriesperiodic) to '/Users/emmarocheteau/PycharmProjects/catherine/eICU_data/timeseriesperiodic.csv' with csv header
-\copy (select * from timeseriesaperiodic) to '/Users/emmarocheteau/PycharmProjects/catherine/eICU_data/timeseriesaperiodic.csv' with csv header
+\copy (select * from labels as l where l.patientunitstayid in (select * from timeseries_patients)) to '/Users/asgnxt/hcnet/eICU-GNN-LSTM/eICU_data/labels.csv' with csv header
+\copy (select * from diagnoses as d where d.patientunitstayid in (select * from timeseries_patients)) to '/Users/asgnxt/hcnet/eICU-GNN-LSTM/eICU_data/diagnoses.csv' with csv header
+\copy (select * from flat as f where f.patientunitstayid in (select * from timeseries_patients)) to '/Users/asgnxt/hcnet/eICU-GNN-LSTM/eICU_data/flat_features.csv' with csv header
+\copy (select * from timeserieslab) to '/Users/asgnxt/hcnet/eICU-GNN-LSTM/eICU_data/timeserieslab.csv' with csv header
+\copy (select * from timeseriesresp) to '/Users/asgnxt/hcnet/eICU-GNN-LSTM/eICU_data/timeseriesresp.csv' with csv header
+\copy (select * from timeseriesperiodic) to '/Users/asgnxt/hcnet/eICU-GNN-LSTM/eICU_data/timeseriesperiodic.csv' with csv header
+\copy (select * from timeseriesaperiodic) to '/Users/asgnxt/hcnet/eICU-GNN-LSTM/eICU_data/timeseriesaperiodic.csv' with csv header
